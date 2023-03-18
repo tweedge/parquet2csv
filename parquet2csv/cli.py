a96da996c2bb0d2404a4563e331ac3aa6babe832
Chris Partridge <chris@partridge.tech>
2023-03-18 11:15:50 -0400

    Try memory efficient r/w

--- parquet2csv/cli.py
+++ parquet2csv/cli.py
@@ -1,13 +1,14 @@
-import pandas
+import pyarrow.parquet as pq
 import argparse
 
 description = (
-    "Dumps a given Parquet file to CSV."
+    "Dumps a given Parquet file to CSV (efficiently)."
 )
 
 def main():
     parser = argparse.ArgumentParser(description=description)
 
+    # user gives Parquet file and CSV file
     parser.add_argument(
         "--parquet",
         required=True,
@@ -18,7 +19,34 @@ def main():
         required=True,
         help="The path to the CSV file output the dump to",
     )
+    parser.add_argument(
+        "--chunk-size",
+        required=False,
+        help="How many rows to read/write at a time",
+        type=int,
+        default=10000,
+    )
     args = parser.parse_args()
 
-    dataframe = pandas.read_parquet(args.parquet)
-    dataframe.to_csv(args.csv)
+    # create a Parquet file reader
+    parquet_reader = pq.ParquetFile(args.parquet)
+
+    # get the number of rows in the Parquet file
+    num_rows = parquet_reader.metadata.num_rows
+
+    # create a CSV file writer
+    csv_writer = open(args.csv, 'w')
+
+    # write the column names to the CSV file
+    schema = parquet_reader.schema
+    csv_writer.write(','.join(schema.names) + '\n')
+
+    # read and write the Parquet file in chunks
+    for i in range(0, num_rows, args.chunk_size):
+        table = parquet_reader.read_row_group(i, min(args.chunk_size, num_rows - i))
+        df = table.to_pandas()
+        df.to_csv(csv_writer, header=False, index=False)
+        del table, df
+
+    # close the CSV file writer
+    csv_writer.close()
